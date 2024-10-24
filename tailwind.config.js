8bf06152d7b2af6868f5422623db2c1ac5f4508e
Brian Qi <qi116@purdue.edu>
2024-10-24 15:21:05 -0400

    Added standardized colors to tailwind.config.js. Improved look of login and sign up pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,25 @@ module.exports = {
     "./src/frontend/screens/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ffBackground': '#f6f6f6',
+        'ffCard': '#ffffff',
+        'ffEdge': '#dddddd',
+        'ffHeading': '#111827',
+        'ffBody': '#6b7280',
+        'ffText': '#000000',
+        'ffRedL': '#b51536',
+        'ffGreenL': '#079373',
+        'ffGreyL': '#8c8c9c',
+        'ffRedD': '#73142e',
+        'ffGreenD': '#184941',
+        'ffGreyD': '#41545b',
+      }
+    },
   },
   plugins: [],
   
+  
 };
+
