8041c6cee89b48d34902548247ad383a3efcedee
shreyashbairi <sbairi@purdue.edu>
2024-11-27 17:57:40 -0500

    cehck

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export const GAPIKEY = process.env.REACT_APP_GAPI_KEY;
-<<<<<<< HEAD
-export const API_BASE_URL = 'https://feastfinder-vn6l.onrender.com' || 'http://localhost:5001';
-=======
-export const API_BASE_URL = 'https://feastfinder-bzq3.onrender.com';
->>>>>>> master
+export const API_BASE_URL = 'http://localhost:5001';
 console.log('API_BASE_URL:', API_BASE_URL);
+
+//'https://feastfinder-bzq3.onrender.com' ||
